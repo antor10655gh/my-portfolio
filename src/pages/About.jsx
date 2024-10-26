4dc7981a6b1ac7e340374f7fbf1a7c3274e4b264
AM Antor <creativefreelancerantor@gmail.com>
2024-10-26 13:03:54 +0600

    add homeAbout section in About page

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,11 +1,13 @@
 // eslint-disable-next-line no-unused-vars
 import React from "react";
 import PageHeading from "../components/shared/PageHeading";
+import HomeAbout from "../components/home/HomeAbout";
 
 const About = () => {
   return (
     <div>
       <PageHeading pageName="About" />
+      <HomeAbout />
     </div>
   );
 };
