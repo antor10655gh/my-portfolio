e3c731362de4fd1f562e20edba6292dd147088e8
AM Antor <creativefreelancerantor@gmail.com>
2024-10-26 15:51:34 +0600

    add background animation

--- src/components/about/LetsDo.jsx
+++ src/components/about/LetsDo.jsx
@@ -1,6 +1,5 @@
 import React from "react";
 import banner from "../../assets/images/contactBG.png";
-import { TypeAnimation } from "react-type-animation";
 import PrimaryButton from "../primarybutton/PrimaryButton";
 import { useNavigate } from "react-router-dom";
 
@@ -13,16 +12,16 @@ const LetsDo = () => {
 
   return (
     <div className="w-full px-4 lg:px-0 py-[40px] md:py-[70px] lg:py-[100px]">
-      <div className="w-full h-full px-4 lg:px-0 text-white overflow-hidden">
+      <div className="w-full h-full text-white overflow-hidden">
         <div
-          className="max-w-screen-xl mx-auto h-full  flex flex-col justify-center rounded-md text-center py-[50px] md:py-[70px] lg:py-[100px]"
+          className="max-w-screen-xl mx-auto h-full flex flex-col justify-center rounded-md text-center py-[50px] md:py-[70px] lg:py-[100px] bg-animated"
           style={{
-            background: `url(${banner}) no-repeat center center / cover`,
+            backgroundImage: `url(${banner})`,
           }}
         >
           <div>
             <p>Get Started</p>
-            <h1 className="text-[24px] md:text-[34px] lg:text-[44px uppercase font-bold my-3">
+            <h1 className="text-[24px] md:text-[34px] lg:text-[44px] uppercase font-bold my-3">
               Let’s do something <br /> new with me!
             </h1>
             <div className="flex gap-4 items-center justify-center">
